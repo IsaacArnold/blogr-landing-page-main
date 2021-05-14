dcb3c8df27f1336bc29744d7ef987b49868da3d6
Isaac Arnold <isaacarnold@icloud.com>
2021-05-15 11:02:55 +1200

    Adds initial styling to mobile nav

--- script.js
+++ script.js
@@ -2,18 +2,30 @@
 Global Variables
 =====================================*/
 const laptopLink = document.querySelectorAll(".laptop-link");
+const hamburgerNavDiv = document.querySelector(".logo-hamburger");
+const hamburgerIcon = hamburgerNavDiv.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
 
 /* ====================================
 Event Listeners
 =====================================*/
+// Desktop navigation
 laptopLink.forEach((link) => {
   let chevron = link.querySelector(".chevron");
   let linkUl = link.querySelector(".laptop-link-ul");
   link.addEventListener("click", (e) => {
-    console.log(e.target);
     if (e.target === chevron || e.target === link) {
       linkUl.classList.toggle("hide");
       chevron.classList.toggle("chevron-up");
     }
   });
+});
+
+// Mobile navigation
+hamburgerNavDiv.addEventListener("click", (e) => {
+  if (e.target === hamburgerIcon) {
+    console.log("success");
+    mobileNav.classList.toggle("hide");
+    console.log(mobileNav);
+  }
 });
