f402e47109fbea7a59afe152df5fdd78a7732c1e
Isaac Arnold <isaacarnold@icloud.com>
2021-05-14 16:51:58 +1200

    Adds dropdown functionality for main nav ul links

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const laptopNav = document.querySelector(".laptop-nav");
-const laptopNavUl = document.querySelector(".laptop-nav-ul");
-const laptopNavUlItems = laptopNavUl.querySelector("ul li");
-const chevron = document.querySelector(".chevron");
+/* ====================================
+Global Variables
+=====================================*/
+const laptopLink = document.querySelectorAll(".laptop-link");
 
-console.log(laptopNav);
-console.log(laptopNavUl);
-
-laptopNav.addEventListener("click", (e) => {
-  if (e.target === chevron) {
-    console.log("chevron");
-  }
-  if (e.target === laptopNavUlItems) {
-    console.log("ul");
-  }
+/* ====================================
+Event Listeners
+=====================================*/
+laptopLink.forEach((link) => {
+  let chevron = link.querySelector(".chevron");
+  let linkUl = link.querySelector(".laptop-link-ul");
+  link.addEventListener("click", (e) => {
+    console.log(e.target);
+    if (e.target === chevron || e.target === link) {
+      linkUl.classList.toggle("hide");
+    }
+  });
 });
